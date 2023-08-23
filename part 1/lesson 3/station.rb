6eeda346acfb2db9bb0d51d91a5937522f6a30d9
eiz <sashkaseb@mail.ru>
2023-08-23 21:26:05 +0300

    Исправил первое задание

--- part 1/lesson 3/station.rb
+++ part 1/lesson 3/station.rb
@@ -1,23 +1,23 @@
 class Station
-  @@trains = {}
 
   def initialize(name)
     @name = name
+    @trains = {}
   end
 
   def accept_train(train_name, train_type)
-    @@trains[train_name] = train_type
+    @trains[train_name] = train_type
   end
 
   def show_trains
-    @@trains.each{|k, v| puts "Поезд #{k} типа #{v} находится в данный момент на станции"}
+    @trains.each{|k, v| puts "Поезд #{k} типа #{v} находится в данный момент на станции"}
   end
 
-  def number_of_train_types(type)
-    @@trains.values.count(type)
+  def trains_by_type(type)
+    @trains.values.count(type)
   end
 
   def send_a_train(name_to_send)
-    @@trains.delete(name_to_send)
+    @trains.delete(name_to_send)
   end
 end
